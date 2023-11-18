0901459dd3978c15f093f9c52b425441f3ffac89
Scott Zeta <wc070211@gmail.com>
2023-11-18 19:30:17 +1030

    approaching to convert to json, mid checkpoint

--- table_parser.py
+++ table_parser.py
@@ -1,10 +1,27 @@
 def read_table(filename):
    with open(filename, "r") as f:
        content = f.read()
-       print(content)
+       return content
 
+def modify_for_json(content):
+    linesArr = []
+    lines = content.splitlines()
+    for i in range(len(lines)):
+        line = lines[i]
+        if line.rstrip() and not line.rstrip().endswith("{") and lines[i+1].rstrip() != '}':
+            line += ','
+        else:   
+            print(lines[i])
+        line = line.replace("=",":")
+        linesArr.append(line)
+    linesArr.insert(0,"{")
+    linesArr.append("}")
+    newContent = "\n".join(linesArr)
+    # print(newContent)
+    
 def main():
-    read_table("./vanilla_resource/13_australasia.txt")
-        
+    content = read_table("./vanilla_resource/13_australasia.txt")
+    modify_for_json(content)
+    
 if __name__ == "__main__":
     main()
