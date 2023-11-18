f3f41bf5c47122a40014dda7edd34388240fc0c8
Scott Zeta <wc070211@gmail.com>
2023-11-18 20:46:19 +1030

    convert to json

--- table_parser.py
+++ table_parser.py
@@ -1,27 +1,52 @@
+import re
+import json
+
 def read_table(filename):
-   with open(filename, "r") as f:
+   with open(filename, "r",encoding='utf-8-sig') as f:
        content = f.read()
        return content
 
 def modify_for_json(content):
     linesArr = []
     lines = content.splitlines()
+    arrayRegex = re.compile(r'\{.*?\}')
+    keyRegex = re.compile(r'^\s*([\w]+)\s*=', re.MULTILINE)
     for i in range(len(lines)):
-        line = lines[i]
-        if line.rstrip() and not line.rstrip().endswith("{") and lines[i+1].rstrip() != '}':
+        line = lines[i].rstrip()
+        
+        # convert array to json array
+        if arrayRegex.search(line):
+            # this line deal with some customized array
+            line = line.replace('state_trait_new_world','"state_trait_new_world"')
+            line = line.replace("{","[").replace("}","]")
+            line = line.replace('" "', '", "')
+        
+        # convert key to json key
+        key = keyRegex.findall(line)
+        if key:
+            line = line.replace(key[0],'"' + key[0] + '"')
+        
+        # add comma to the end of element except the last one
+        is_last_in_block = i + 1 < len(lines) and i + 2 < len(lines) and lines[i + 1].strip() != '}'
+        if line and not line.endswith("{") and is_last_in_block:
             line += ','
-        else:   
-            print(lines[i])
+        
+        # replace = with :
         line = line.replace("=",":")
         linesArr.append(line)
     linesArr.insert(0,"{")
     linesArr.append("}")
-    newContent = "\n".join(linesArr)
-    # print(newContent)
+    jsonContent = "\n".join(linesArr)
+    return jsonContent
     
 def main():
     content = read_table("./vanilla_resource/13_australasia.txt")
-    modify_for_json(content)
+    jsonString = modify_for_json(content)
+    # print(jsonString)
+    with open('string.txt', 'w') as f:
+        f.write(jsonString)
+    jsonData = json.loads(jsonString)
+    print(jsonData)
     
 if __name__ == "__main__":
     main()
