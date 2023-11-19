52026e205c59f7aac4a2509ca8145fc3a366e79e
Scott Zeta <wc070211@gmail.com>
2023-11-19 17:53:19 +1030

    unquote key and replace symbols

--- stringfyer.py
+++ stringfyer.py
@@ -5,9 +5,25 @@ def stringfy(jsonData):
     # Stringfy the json data back to raw original table
     s = json.dumps(jsonData, indent=4)
     # Need additional Stringfy Implementation
-    # put all elements in array in one line
+    
+    # put all elements in array in one line, and replace comma with space
     arrayRegex = r'\[\s+([^]]+?)\s+\]'
     s = re.sub(arrayRegex, lambda m: '[' + m.group(1).replace('\n', '').replace('  ', '') + ']', s)
+    s = s.replace('","', '" "')
+    
+    # unquote the key
+    keyRegex = re.compile(r'"\s*([\w]+)\s*"\s*:')
+    s = keyRegex.sub(r'\1 :',s)
+    
+    # replace [] with {}
+    s = s.replace('[','{ ').replace(']',' }')
+    
+    # replace all : with =
+    s = s.replace(':','=')
+    
+    # remove all , 
+    s = s.replace(',','')
+    
     
     print(s)
     with open('debugOutput/stringfy.txt', 'w') as f:
