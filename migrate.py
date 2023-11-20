5ec0202d783bc23f265b2d266bd62f9e37050aca
Scott Zeta <wc070211@gmail.com>
2023-11-20 15:29:04 +1030

    handle with capped, arable resource migrates

--- migrate.py
+++ migrate.py
@@ -22,9 +22,26 @@ def migrate(historical_data, vanilla_data):
         if(modified_data[k]):
             #initial new capped_resources, arable_resources, arable_land for modfied_data
             mod_capped_resources = {}
-            mod_arable_resources = []
+            mod_arable_resources = set()
             mod_arable_land = 0
             #iterate the capped_resources in historical_data
             for resource,number in historical_data[k]['capped_resources'].items():
-                print(resource)
+                # if the resource belongs to arable_resources_list, 
+                # add it to mod_arable_resources follow vanilla game setting
+                if resource in arable_resources_list:
+                    #deal with two special custmized ranche cases
+                    if resource == "bg_cattle_ranches" or resource == "bg_sheep_ranches":
+                        resource = "bg_livestock_ranches"
+                    mod_arable_resources.add(resource)
+                    # the number add to sum of mod_arable_land
+                    mod_arable_land += number
+                else:
+                    # for other resources, add key value pairs to mod_capped_resources
+                    mod_capped_resources[resource] = number
+            # conver set to list for json output
+            mod_arable_resources = list(mod_arable_resources)
+            # assign moded value to modfied_data
+            modified_data["capped_resources"] = mod_capped_resources
+            modified_data["arable_resources"] = mod_arable_resources
+            modified_data["arable_land"] = mod_arable_land
     return modified_data
