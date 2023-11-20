1596196c06f98f5dbbe241acb9d709f7602e6569
Scott Zeta <wc070211@gmail.com>
2023-11-20 14:49:36 +1030

    define the list of arable_resources

--- migrate.py
+++ migrate.py
@@ -1,3 +1,19 @@
+from typing import List
+
+arable_resources_list:List[str] = [
+    # Agriculture
+    "bg_rye_farms", "bg_wheat_farms", "bg_rice_farms", 
+    "bg_maize_farms", "bg_millet_farms", "bg_vineyard_plantations",
+    # Ranching
+    "bg_livestock_ranches",
+    #custmised ranching
+    "bg_cattle_ranches", "bg_sheep_ranches"
+    # Plantation
+    "bg_coffee_plantations", "bg_cotton_plantations", "bg_silk_plantations",
+    "bg_dye_plantations", "bg_opium_plantations", "bg_tea_plantations",
+    "bg_tobacco_plantations", "bg_sugar_plantations", "bg_banana_plantations"
+    ]
+
 def migrate(historical_data, vanilla_data):
     modified_data = vanilla_data
     return modified_data
