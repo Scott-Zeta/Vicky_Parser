13d948959da270e930fc1343471d87564fccd7c9
Scott Zeta <wc070211@gmail.com>
2023-11-22 18:04:57 +1030

    tokenizer complete

--- tokenizer.py
+++ tokenizer.py
@@ -5,7 +5,7 @@ def tokenizer(f):
         tokens = [""]
         quote_flag = False
         
-        def parse_quto(tokens, char, quote_flag):
+        def parse_quote(tokens, char, quote_flag):
             # if no quote flag, and tokens[-1] is not empty,
             # start a new token
             if not quote_flag and tokens[-1]:
@@ -18,23 +18,51 @@ def tokenizer(f):
             
         for line in lines:
             for char in line:
-                # quote case
-                if char == '"':
-                    quote_flag = parse_quto(tokens, char, quote_flag)
-                # other char out of the quote
-                elif not quote_flag:
-                    pass
-                # case inside the quote, add them all
-                else:
-                    tokens[-1] += char
-                pass
+                try:
+                    # quote case
+                    if char == '"':
+                        quote_flag = parse_quote(tokens, char, quote_flag)
+                    # other char out of the quote
+                    elif not quote_flag:
+                        # comment case, drop everything include #
+                        if char == '#':
+                            break
+                        # operator case, add as a new token
+                        # operators are always as a single token
+                        elif char in ('=', '{', '}', ':'):
+                            if tokens[-1]:
+                                tokens.append(char)
+                                tokens.append("")
+                            else:
+                                tokens[-1] += char
+                                tokens.append("")
+                        # space, tab, newline case, 
+                        # all count as the end of the current token
+                        elif char.isspace():
+                            if tokens[-1]:
+                                tokens.append("")
+                        # other char, add to the current token
+                        else:
+                            tokens[-1] += char
+                    # case inside the quote, add them all
+                    else:
+                        tokens[-1] += char
+                except Exception as e:
+                    print(f"Error on char {char} in line {line}")
+                    print(f"Current tokens: {tokens}")
+                    print(f"Current quote flag: {quote_flag}")
+                    print(e)
+        # drop all duplicate empty token
+        while not tokens[-1]:
+            tokens.pop()
         return tokens
-    return
+    return read_tokens(lines)
 
 
 def main():
-    with open("./13_australasia.txt", "r", encoding='utf-8-sig') as f:
+    with open("./vanilla_resource/13_australasia.txt", "r", encoding='utf-8-sig') as f:
         token = tokenizer(f)
+        print(token)
     # for k, v in token.items():
     #     print(k)
     # print(token["wealth_1"])
